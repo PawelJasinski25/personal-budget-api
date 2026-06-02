ed32a2df13c634c3bb7f224263291a1ecf80e4b4
PawelJasinski25 <paweljasinski257@gmail.com>
2026-06-02 23:18:37 +0200

    initialize project structure

---
+++ src/main/java/com/jasinskipawel/personal_budget_api/PersonalBudgetApiApplication.java
@@ -0,0 +1,13 @@
+package com.jasinskipawel.personal_budget_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBudgetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBudgetApiApplication.class, args);
+	}
+
+}
