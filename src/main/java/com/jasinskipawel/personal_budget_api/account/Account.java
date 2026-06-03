aab6a589834abda41ca76d71c3c2eac864906b44
PawelJasinski25 <paweljasinski257@gmail.com>
2026-06-03 12:17:40 +0200

    add account entity

---
+++ src/main/java/com/jasinskipawel/personal_budget_api/account/Account.java
@@ -0,0 +1,50 @@
+package com.jasinskipawel.personal_budget_api.account;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private BigDecimal balance;
+
+    public Account() {}
+
+    public Account(String name, BigDecimal balance) {
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+}
